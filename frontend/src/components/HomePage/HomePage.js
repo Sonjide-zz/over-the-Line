39ed1440769d1870592d944fe6ba837b3a28fa7f
Alex <alex.andrul10@gmail.com>
2020-08-26 19:43:18 +0100

    change element homepage

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -8,22 +8,24 @@ import "./HomePage.css";
 
 export const HomePage = () => {
   return (
-    <Element name="homepage" className="element">
+    <>
       <NavBar />
-      <div className="homepage">
-        <Link
-          className="h1-link"
-          activeClass="active"
-          to="artists"
-          spy={true}
-          smooth={true}
-          offset={0}
-          duration={1500}
-        >
-          <h1 className="h1-over-the-line">Over the line</h1>
-        </Link>
-      </div>
-      <Footer />
-    </Element>
+      <Element name="homepage" className="element">
+        <div className="homepage">
+          <Link
+            className="h1-link"
+            activeClass="active"
+            to="artists"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={1500}
+          >
+            <h1 className="h1-over-the-line">Over the line</h1>
+          </Link>
+        </div>
+        <Footer />
+      </Element>
+    </>
   );
 };
